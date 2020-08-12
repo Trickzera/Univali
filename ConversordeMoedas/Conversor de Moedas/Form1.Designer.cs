4f8be7ea051ecc3803201e2c6a8d7538128ad9e2
Trickzera <69552004+Trickzera@users.noreply.github.com>
2020-08-12 20:50:54 -0300

    ConversordeMoedasPatrick

---
+++ ConversordeMoedas/Conversor de Moedas/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Conversor_de_Moedas_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.real1 = new System.Windows.Forms.TextBox();
+            this.dolar1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.euro1 = new System.Windows.Forms.TextBox();
+            this.real2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.euro2 = new System.Windows.Forms.TextBox();
+            this.dolar2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Real p/ Dólar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dólar p/ Real";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // real1
+            // 
+            this.real1.Location = new System.Drawing.Point(12, 65);
+            this.real1.Name = "real1";
+            this.real1.Size = new System.Drawing.Size(168, 20);
+            this.real1.TabIndex = 2;
+            // 
+            // dolar1
+            // 
+            this.dolar1.Location = new System.Drawing.Point(12, 120);
+            this.dolar1.Name = "dolar1";
+            this.dolar1.Size = new System.Drawing.Size(168, 20);
+            this.dolar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dólar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Real";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Real p/ Euro";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(276, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Euro p/ Real";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // euro1
+            // 
+            this.euro1.Location = new System.Drawing.Point(193, 120);
+            this.euro1.Name = "euro1";
+            this.euro1.Size = new System.Drawing.Size(161, 20);
+            this.euro1.TabIndex = 8;
+            // 
+            // real2
+            // 
+            this.real2.Location = new System.Drawing.Point(193, 65);
+            this.real2.Name = "real2";
+            this.real2.Size = new System.Drawing.Size(161, 20);
+            this.real2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Real";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Euro";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(360, 91);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Dólar p/ Euro";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(447, 91);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Euro p/ Dólar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // euro2
+            // 
+            this.euro2.Location = new System.Drawing.Point(360, 120);
+            this.euro2.Name = "euro2";
+            this.euro2.Size = new System.Drawing.Size(167, 20);
+            this.euro2.TabIndex = 14;
+            // 
+            // dolar2
+            // 
+            this.dolar2.Location = new System.Drawing.Point(361, 65);
+            this.dolar2.Name = "dolar2";
+            this.dolar2.Size = new System.Drawing.Size(166, 20);
+            this.dolar2.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(426, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Dólar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(426, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Euro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dolar2);
+            this.Controls.Add(this.euro2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.real2);
+            this.Controls.Add(this.euro1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dolar1);
+            this.Controls.Add(this.real1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox real1;
+        private System.Windows.Forms.TextBox dolar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox euro1;
+        private System.Windows.Forms.TextBox real2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox euro2;
+        private System.Windows.Forms.TextBox dolar2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ConversordeMoedas/Conversor de Moedas/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor_de_Moedas_2
+{
+    public partial class Form1 : Form
+    {
+        double valor = 0;
+        double taxa1 = 5.44;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(real1.Text) / taxa1;
+            dolar1.Text = valor.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            valor = taxa1 * double.Parse(dolar1.Text);
+            real1.Text = valor.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        double taxa2 = 6.41;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(real2.Text) / taxa2;
+            euro1.Text = valor.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            valor = taxa2 * double.Parse(euro1.Text);
+            real2.Text = valor.ToString();
+        }
+        double taxa3 = 1.18;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(dolar2.Text) / taxa3;
+            euro2.Text = valor.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            valor = taxa3 * double.Parse(euro2.Text);
+            dolar2.Text = valor.ToString();
+        }
+    }
+}
